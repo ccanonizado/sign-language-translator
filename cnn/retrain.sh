91d67bd0c6e77584252d11047858bcde84b8791c
ccanonizado <cecanonizado@up.edu.ph>
2019-03-19 16:26:25 +0800

    Edit retrain script and git ignore

--- cnn/retrain.sh
+++ cnn/retrain.sh
@@ -1,5 +1,7 @@
 python retrain.py \
     --image_dir="data" \
+    --model_dir="inception" \
+    --bottleneck_dir="bottlenecks" \
     --how_many_training_step="500" \
     --output_graph="retrained_graph.pb" \
     --output_labels="labels.txt" \
