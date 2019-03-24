d80a2385a0a557a22d03e635892dc3733b60a857
ccanonizado <cecanonizado@up.edu.ph>
2019-03-24 14:25:27 +0800

    Edit git ignore and add summaries

--- cnn/retrain.sh
+++ cnn/retrain.sh
@@ -5,4 +5,5 @@ python retrain.py \
     --how_many_training_step="4000" \
     --output_graph="pb/retrained_graph.pb" \
     --output_labels="labels.txt" \
-    --final_tensor_name="final_result"
+    --final_tensor_name="final_result" \
+    --summaries_dir="summaries" 
