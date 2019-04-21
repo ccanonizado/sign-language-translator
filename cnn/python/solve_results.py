b0048ebb87d68528763ef70c6eab41247c0bb957
ccanonizado <cecanonizado@up.edu.ph>
2019-04-22 02:53:40 +0800

    Add computation of other averages

--- cnn/python/solve_results.py
+++ cnn/python/solve_results.py
@@ -7,12 +7,16 @@ f2 = open("../results/results_final.txt", "w")
 
 labels = ["a", "b", "c", "d", "e", "f", "g", "h", "hellohi", "i", "j", "k", "l", "m", "n", "no", "o", "okaygoodjob", "p", "q", "r", "s", "t", "that", "u", "v", "w", "x", "y", "yes", "z"]
 
+average_confidence = 0
+average_top_1 = 0
+average_top_5 = 0
+
 # number of labels
 for i in range(0,len(labels)):
 
   f2.write("{}:\n".format(labels[i]))
 
-  average = 0
+  confidence = 0
   top_1_count = 0
   top_5_count = 0
   wrong_predictions = {}
@@ -34,7 +38,7 @@ for i in range(0,len(labels)):
         top_5_count += 1
         
         # the value contains a newline - split again
-        average += float(line[1].split("\n")[0])
+        confidence += float(line[1].split("\n")[0])
 
       # if the prediction was wrong - add to dictionary
       elif label != line[0] and k == 0:      
@@ -44,8 +48,8 @@ for i in range(0,len(labels)):
           wrong_predictions[line[0]] = 1
 
   # compute predictions
-  if average != 0:
-    average = (average / 5) * 100
+  if confidence != 0:
+    confidence = (confidence / 5) * 100
 
   if top_1_count != 0:
     top_1_count = (top_1_count / 5) * 100
@@ -53,8 +57,13 @@ for i in range(0,len(labels)):
   if top_5_count != 0:
     top_5_count = (top_5_count / 5) * 100
 
+  # add to averages' sum
+  average_confidence += confidence
+  average_top_1 += top_1_count
+  average_top_5 += top_5_count
+
   # print results
-  f2.write("Confidence: {}\n".format(format(average, '.4f')))
+  f2.write("Confidence: {}\n".format(format(confidence, '.4f')))
   f2.write("Top-1-Accuracy: {}\n".format(top_1_count))
   f2.write("Top-5-Accuracy: {}\n".format(top_5_count))
   f2.write("Wrong Predictions: \n")
@@ -66,6 +75,15 @@ for i in range(0,len(labels)):
   f2.write(wrong_string)
 
   f2.write("==================================================\n")
-    
+
+# compute averages
+average_confidence /= 31
+average_top_1 /= 31
+average_top_5 /= 31
+
+f2.write("Confidence Average: {}\n".format(format(average_confidence, '.4f')))
+f2.write("Top-1-Accuracy Average: {}\n".format(format(average_top_1, '.4f')))
+f2.write("Top-5-Accuracy Average: {}\n".format(format(average_top_5, '.4f')))
+
 f1.close()
 f2.close()
